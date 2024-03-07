339200742f979997c245b62490083e79d313ebff
Sandeep <sandeepwadhawan2001@gmail.com>
2024-03-07 23:28:00 +0530

    Learnt about Props using TailwindCSS

---
+++ 03_PropsWithTailwind/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const myArr = [12, 23, 34];
+  const myObj = {
+    Name: "Sandeep",
+    Age: 22,
+    Hobby: "Development and Singing"
+  }
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind Test</h1>
+      <Card userName="Sandeep Wadhawan" myArr={[12, 32, 1]} obj={myObj} src="https://avatars.githubusercontent.com/u/68498988?v=4" designation="Software Developer" location=" (New Delhi, India)"/>
+      <Card userName="Hitesh Choudhary" src="https://avatars.githubusercontent.com/u/11613311?v=4" designation="Software Engineer" location=" (Bangalore, India)"/>
+    </>
+  )
+}
+
+export default App
---
+++ 03_PropsWithTailwind/src/components/Card.jsx
@@ -0,0 +1,60 @@
+// Type rfce for boiler-plate generation of react. rfce --> Create React Functional Component and export
+
+import React from 'react'
+
+function Card({ userName: name, src, designation: des, location: loc}) {
+    console.log("Username : ", name);
+    console.log("Designation : ", des);
+    console.log("Location : ", loc);
+
+    return (
+        <>
+            <div className="m-6">
+                <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+                    <img className="w-24 h-24 rounded-full mx-auto" src={src} alt="" width="384" height="512" />
+                    <div className="pt-6 space-y-4">
+                        <blockquote>
+                            <p className="text-lg font-medium">
+                                “Tailwind CSS is the only framework that I've seen scale
+                                on large teams. It’s easy to customize, adapts to any design,
+                                and the build size is tiny.”
+                            </p>
+                        </blockquote>
+                        <figcaption className="font-medium">
+                            <div>
+                                {name}
+                            </div>
+                            <div>
+                                {des} {loc}
+                            </div>
+                        </figcaption>
+                    </div>
+                </figure>
+            </div>
+            {/* <div className="m-6">
+                <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+                    <img className="w-24 h-24 rounded-full mx-auto" src="https://avatars.githubusercontent.com/u/11613311?v=4" alt="" width="384" height="512" />
+                    <div className="pt-6 space-y-4">
+                        <blockquote>
+                            <p className="text-lg font-medium">
+                                “Tailwind CSS is the only framework that I've seen scale
+                                on large teams. It’s easy to customize, adapts to any design,
+                                and the build size is tiny.”
+                            </p>
+                        </blockquote>
+                        <figcaption className="font-medium">
+                            <div>
+                                Hitesh Choudhary
+                            </div>
+                            <div>
+                                Software Engineer, India
+                            </div>
+                        </figcaption>
+                    </div>
+                </figure>
+            </div> */}
+        </>
+    );
+}
+
+export default Card
